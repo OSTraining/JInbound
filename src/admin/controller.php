8678a933053b09f5af82a3937d9c526275e7227c
Bill Tomczak <bill@ostraining.com>
2018-03-19 15:11:05 -0700

    Remove obsolete help link

--- src/admin/controller.php
+++ src/admin/controller.php
@@ -19,26 +19,7 @@ defined('JPATH_PLATFORM') or die;
 
 class JInboundController extends JInboundBaseController
 {
-    public function display($cachable = false, $urlparams = false)
-    {
-        $app        = JFactory::getApplication();
-        $view       = $app->input->get('view', 'Dashboard', 'cmd');
-        $helpurl    = JInbound::config('help_url');
-        $configured = preg_match('/^https?\:\/{2}/', $helpurl);
-        if (!$configured) {
-            $app->enqueuemessage(JText::_('COM_JINBOUND_SAVE_CONFIG_WARNING'), 'warning');
-        }
-        // the help view acts as a redirect to the REAL help page
-        // we only really use this in the main component submenu,
-        // as any link we can handle via code will just use the config option
-        if ('help' == strtolower($view)) {
-            $app->redirect($configured ? JInbound::config('help_url') : 'index.php?option=com_jinbound');
-            // tear down the application
-            jexit();
-        }
-        $app->input->set('view', $view);
-        parent::display($cachable);
-    }
+    protected $default_view = 'dashboard';
 
     /**
      * @return void
