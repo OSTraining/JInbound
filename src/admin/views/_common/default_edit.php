5d8204c4b85533979ac86ec19f51d0ef28cee698
Bill Tomczak <bill@ostraining.com>
2018-03-23 15:36:40 -0700

    Reformat for standards

--- src/admin/views/_common/default_edit.php
+++ src/admin/views/_common/default_edit.php
@@ -17,28 +17,38 @@
 
 defined('JPATH_PLATFORM') or die;
 
+$app = JFactory::getApplication();
+
 ?>
-<div id="jinbound_component" class="row-fluid <?php echo $this->viewClass; ?>">
-    <form action="<?php echo JInboundHelperUrl::_(); ?>" method="post" id="adminForm" name="adminForm"
-          class="form-validate" enctype="multipart/form-data">
-        <fieldset>
-            <?php echo $this->loadTemplate('edit_default'); ?>
-            <input type="hidden" name="task" value=""/>
-            <input type="hidden" name="id" value="<?php echo (int)@$this->item->id; ?>"/>
-            <input type="hidden" name="function" value="<?php echo JRequest::getCmd('function'); ?>"/>
-            <?php if ('component' == JRequest::getCmd('tmpl')) : ?>
-                <input type="hidden" name="tmpl" value="component"/>
-                <input type="hidden" name="layout" value="modal"/>
-            <?php endif; ?>
-            <?php echo JHtml::_('form.token'); ?>
-        </fieldset>
-        <?php echo $this->loadTemplate('edit_tabs'); ?>
-    </form>
-</div>
-<?php echo $this->loadTemplate('footer'); ?>
-<?php if (JInbound::config("debug", 0)) : ?>
+    <div id="jinbound_component" class="row-fluid <?php echo $this->viewClass; ?>">
+        <form action="<?php echo JInboundHelperUrl::_(); ?>" method="post" id="adminForm" name="adminForm"
+              class="form-validate" enctype="multipart/form-data">
+            <fieldset>
+                <?php echo $this->loadTemplate('edit_default'); ?>
+                <input type="hidden" name="task" value=""/>
+                <input type="hidden" name="id" value="<?php echo (int)@$this->item->id; ?>"/>
+                <input type="hidden" name="function" value="<?php echo $app->input->getCmd('function'); ?>"/>
+                <?php
+                if ('component' == $app->input->getCmd('tmpl')) :
+                    ?>
+                    <input type="hidden" name="tmpl" value="component"/>
+                    <input type="hidden" name="layout" value="modal"/>
+                <?php
+                endif;
+                echo JHtml::_('form.token');
+                ?>
+            </fieldset>
+            <?php echo $this->loadTemplate('edit_tabs'); ?>
+        </form>
+    </div>
+<?php
+echo $this->loadTemplate('footer');
+
+if (JInbound::config("debug", 0)) :
+    ?>
     <div class="row-fluid">
         <h3>Item:</h3>
         <pre><?php htmlspecialchars(print_r($this->item)); ?></pre>
     </div>
-<?php endif; ?>
+<?php
+endif;
