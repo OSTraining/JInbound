b65e17f2ea4b7874ba85e4fa1bcb26092dd0c675
Bill Tomczak <bill@ostraining.com>
2018-03-28 18:48:53 -0700

    Fix some issues with router

--- src/site/router.php
+++ src/site/router.php
@@ -17,8 +17,6 @@
 
 defined('_JEXEC') or die;
 
-jimport('joomla.application.categories');
-
 function JinboundBuildRoute(&$query)
 {
     $segments = array();
@@ -31,37 +29,33 @@ function JinboundBuildRoute(&$query)
     } else {
         $menuItem = $menu->getItem($query['Itemid']);
     }
-    $mView = (empty($menuItem->query['view'])) ? null : $menuItem->query['view'];
-    $mId   = (empty($menuItem->query['id'])) ? null : $menuItem->query['id'];
 
-    // we always want to use category/page alias and not menu item
-    // but menu items send us only Itemid & option here
-    // so what we need to do is simply use the menu item to determine defaults
-    // for unset query items before proceeding
-
-    // if the query has these, use them
-    foreach (array('view', 'id') as $var) {
-        $mVar = ${'m' . ucwords($var)};
-        if (isset($query[$var])) {
-            $$var = $query[$var];
-            unset($query[$var]);
-        } // otherwise use the one provided in the menu, if applicable
-        else {
-            if (!empty($mVar)) {
-                $$var = $mVar;
-            }
-        }
+    $view = null;
+    if (!empty($query['view'])) {
+        $view = $query['view'];
+        unset($query['view']);
+    } elseif ($menuItem && !empty($menuItem->query['view'])) {
+        $view = $menuItem->query['view'];
     }
+
+    $id = null;
+    if (!empty($query['id'])) {
+        $id = $query['id'];
+        unset($query['id']);
+    } elseif ($menuItem && !empty($menuItem->query['id'])) {
+        $id = $menuItem->query['id'];
+    }
+
     // check page view
-    if (isset($view) && $view == 'page') {
-        if (false !== strpos($id, ':')) {
+    if ($view == 'page' && $id) {
+        $pid = $id;
+        if (strpos($id, ':') !== false) {
             list ($pid, $tmp) = explode(':', $id, 2);
+
         } else {
-            $pid = (int)$id;
             $tmp = '';
         }
-        $pid = (int)$pid;
-        if ($pid) {
+        if ($pid = (int)$pid) {
             $db = JFactory::getDbo();
             $db->setQuery(
                 $db->getQuery(true)
@@ -75,27 +69,35 @@ function JinboundBuildRoute(&$query)
                     ->from('#__jinbound_pages')
                     ->where($db->quoteName('id') . ' = ' . $pid)
             );
-            $record = $db->loadObject();
 
-            if (empty($tmp)) {
-                $id .= ':' . (empty($record->alias) ? JApplication::stringURLSafe($record->name) : $record->alias);
-            }
-            $categories = JCategories::getInstance('Jinbound');
-            $category   = $categories->get($record->category);
-            if ($category) {
-                $path = $category->getPath();
-                $path = array_reverse($path);
-
-                $array = array();
-                foreach ($path as $cid) {
-                    //list ($cid, $tmp) = explode(':', $cid, 2);
-                    $array[] = $cid;
+            if ($record = $db->loadObject()) {
+                if (empty($tmp)) {
+                    $id .= ':'
+                        . (empty($record->alias)
+                            ? JApplicationHelper::stringURLSafe($record->name)
+                            : $record->alias);
                 }
-                $segments = array_merge($segments, array_reverse($array));
+                $categories = JCategories::getInstance('Jinbound');
+                $category   = $categories->get($record->category);
+                if ($category) {
+                    $path = $category->getPath();
+                    $path = array_reverse($path);
+
+                    $array = array();
+                    foreach ($path as $cid) {
+                        $array[] = $cid;
+                    }
+                    $segments = array_merge($segments, array_reverse($array));
+                }
+
+            } else {
+                $id = null;
             }
         }
-        $segments[] = $id;
-        unset($query['id']);
+
+        if ($id) {
+            $segments[] = $id;
+        }
     }
 
     if (isset($query['layout'])) {
@@ -103,6 +105,7 @@ function JinboundBuildRoute(&$query)
             unset($query['layout']);
         }
     }
+
     return $segments;
 }
 
@@ -110,16 +113,17 @@ function JinboundParseRoute($segments)
 {
     $vars = array();
 
-    //Get the active menu item.
+    /** @var JApplicationSite $app */
     $app  = JFactory::getApplication();
     $menu = $app->getMenu();
     $item = $menu->getActive();
 
-    // bugfix for sef
     if (is_object($item)) {
         $templateStyle = $item->template_style_id;
         if ($templateStyle) {
             JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_templates/tables');
+
+            /** @var TemplatesTableStyle $template */
             $template = JTable::getInstance('Style', 'TemplatesTable');
             $template->load($templateStyle);
             if ($template->id) {
@@ -128,14 +132,14 @@ function JinboundParseRoute($segments)
         }
     }
 
-    // Count route segments
     $count = count($segments);
 
-    // Standard routing for pages.
     if (!isset($item)) {
         $vars['view'] = $segments[0];
         $vars['id']   = $segments[$count - 1];
+
         return $vars;
+
     } else {
         if ('page' == $item->query['view']) {
             $vars['view'] = 'page';
