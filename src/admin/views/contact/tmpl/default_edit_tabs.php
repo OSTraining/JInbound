be1c6437d647f67278fde282022ae0513faacce9
Bill Tomczak <bill@ostraining.com>
2018-07-10 13:04:05 -0700

    Fix contact page tab labels

--- src/admin/views/contact/tmpl/default_edit_tabs.php
+++ src/admin/views/contact/tmpl/default_edit_tabs.php
@@ -18,41 +18,49 @@
 defined('JPATH_PLATFORM') or die;
 
 ?>
-
     <div id="jinbound_default_tabset">
 <?php
 echo JHtml::_('jinbound.startTabSet', 'jinbound_default_tabs', array('active' => 'profile_tab'));
 $templates = array(
-    'default'   => false
-,
+    'default'   => false,
     'campaigns' => 'campaigns'
 );
-foreach ($this->form->getFieldsets() as $name => $fieldset) {
+foreach ($this->form->getFieldsets() as $name => $fieldset) :
     $template = array_key_exists($name, $templates) ? $templates[$name] : 'fields';
-    if ($template) {
-        echo JHtml::_('jinbound.addTab', 'jinbound_default_tabs', $name . '_tab',
-            JText::_('COM_JINBOUND_LEAD_FIELDSET_' . $name, true));
+    if ($template) :
+        $fieldsetLabel = $fieldset->label ?: sprintf('COM_JINBOUND_LEAD_FIELDSET_%s', $name);
+        echo JHtml::_(
+            'jinbound.addTab',
+            'jinbound_default_tabs',
+            $name . '_tab',
+            JText::_($fieldsetLabel, true)
+        );
         $this->_currentFieldsetName = $name;
         echo $this->loadTemplate("edit_tabs_page_$template");
         unset($this->_currentFieldsetName);
         echo JHtml::_('jinbound.endTab');
-    }
+    endif;
+
     $tabs = array();
-    if ('details' === $name) {
+    if ('details' === $name) :
         $tabs[] = 'forms';
-    } else {
-        if ('campaigns' === $name) {
+    else :
+        if ('campaigns' === $name) :
             $tabs[] = 'notes';
             $tabs[] = 'tracks';
-        }
-    }
-    if (!empty($tabs)) {
-        foreach ($tabs as $tab) {
-            echo JHtml::_('jinbound.addTab', 'jinbound_default_tabs', $tab . '_tab',
-                JText::_('COM_JINBOUND_LEAD_FIELDSET_' . $tab, true));
+        endif;
+    endif;
+    if (!empty($tabs)) :
+        foreach ($tabs as $tab) :
+            echo JHtml::_(
+                'jinbound.addTab',
+                'jinbound_default_tabs',
+                $tab . '_tab',
+                JText::_('COM_JINBOUND_LEAD_FIELDSET_' . $tab, true)
+            );
             echo $this->loadTemplate("edit_tabs_page_$tab");
             echo JHtml::_('jinbound.endTab');
-        }
-    }
-}
+        endforeach;
+    endif;
+endforeach;
 echo JHtml::_('jinbound.endTabSet');
